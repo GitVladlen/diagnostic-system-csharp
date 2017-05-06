74621c079a3d3629bc477d0cd05904af8d3036fe
Vladlen <vladlen.fedorenko@gmail.com>
2017-05-06 12:53:53 +0300

    ver 0.1

--- DiagnosticSystem/Form1.Designer.cs
+++ DiagnosticSystem/Form1.Designer.cs
@@ -28,74 +28,266 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cboSheet = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.довідкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.btnOpen = new System.Windows.Forms.Button();
             this.btnClassify = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.cboSheet = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvWorkMode = new System.Windows.Forms.DataGridView();
+            this.btnClassifyWork = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotalSuccessTest = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboSheetWork = new System.Windows.Forms.ComboBox();
+            this.btnOpenWork = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvWorkMode)).BeginInit();
             this.SuspendLayout();
             // 
-            // cboSheet
+            // menuStrip1
             // 
-            this.cboSheet.FormattingEnabled = true;
-            this.cboSheet.Location = new System.Drawing.Point(280, 12);
-            this.cboSheet.Name = "cboSheet";
-            this.cboSheet.Size = new System.Drawing.Size(121, 21);
-            this.cboSheet.TabIndex = 0;
-            this.cboSheet.SelectedIndexChanged += new System.EventHandler(this.cboSheet_SelectedIndexChanged);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.довідкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(668, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // довідкаToolStripMenuItem
+            // 
+            this.довідкаToolStripMenuItem.Name = "довідкаToolStripMenuItem";
+            this.довідкаToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.довідкаToolStripMenuItem.Text = "Довідка";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(669, 499);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.cboSheetWork);
+            this.tabPage1.Controls.Add(this.btnOpenWork);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.btnClassifyWork);
+            this.tabPage1.Controls.Add(this.dgvWorkMode);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(661, 473);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Робочий режим";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lblTotalSuccessTest);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.dataGridView);
+            this.tabPage2.Controls.Add(this.cboSheet);
+            this.tabPage2.Controls.Add(this.btnOpen);
+            this.tabPage2.Controls.Add(this.btnClassify);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(661, 473);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Контрольний режим";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // dataGridView
             // 
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(13, 41);
+            this.dataGridView.Location = new System.Drawing.Point(8, 35);
             this.dataGridView.Name = "dataGridView";
-            this.dataGridView.Size = new System.Drawing.Size(469, 337);
-            this.dataGridView.TabIndex = 1;
-            // 
-            // btnOpen
-            // 
-            this.btnOpen.Location = new System.Drawing.Point(13, 12);
-            this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(75, 23);
-            this.btnOpen.TabIndex = 2;
-            this.btnOpen.Text = "Open";
-            this.btnOpen.UseVisualStyleBackColor = true;
-            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            this.dataGridView.Size = new System.Drawing.Size(645, 401);
+            this.dataGridView.TabIndex = 5;
             // 
             // btnClassify
             // 
-            this.btnClassify.Location = new System.Drawing.Point(407, 12);
+            this.btnClassify.Location = new System.Drawing.Point(484, 442);
             this.btnClassify.Name = "btnClassify";
-            this.btnClassify.Size = new System.Drawing.Size(75, 23);
-            this.btnClassify.TabIndex = 3;
-            this.btnClassify.Text = "Classify";
+            this.btnClassify.Size = new System.Drawing.Size(171, 23);
+            this.btnClassify.TabIndex = 7;
+            this.btnClassify.Text = "Почати класифікацію";
             this.btnClassify.UseVisualStyleBackColor = true;
             this.btnClassify.Click += new System.EventHandler(this.btnClassify_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(8, 6);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(147, 23);
+            this.btnOpen.TabIndex = 6;
+            this.btnOpen.Text = "Відкрити файл даних *.xls";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // cboSheet
+            // 
+            this.cboSheet.FormattingEnabled = true;
+            this.cboSheet.Location = new System.Drawing.Point(482, 11);
+            this.cboSheet.Name = "cboSheet";
+            this.cboSheet.Size = new System.Drawing.Size(171, 21);
+            this.cboSheet.TabIndex = 4;
+            this.cboSheet.SelectedIndexChanged += new System.EventHandler(this.cboSheet_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(398, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Вибрати лист:";
+            // 
+            // dgvWorkMode
+            // 
+            this.dgvWorkMode.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvWorkMode.Location = new System.Drawing.Point(6, 35);
+            this.dgvWorkMode.Name = "dgvWorkMode";
+            this.dgvWorkMode.Size = new System.Drawing.Size(645, 402);
+            this.dgvWorkMode.TabIndex = 6;
+            // 
+            // btnClassifyWork
+            // 
+            this.btnClassifyWork.Location = new System.Drawing.Point(480, 443);
+            this.btnClassifyWork.Name = "btnClassifyWork";
+            this.btnClassifyWork.Size = new System.Drawing.Size(171, 23);
+            this.btnClassifyWork.TabIndex = 8;
+            this.btnClassifyWork.Text = "Почати класифікацію";
+            this.btnClassifyWork.UseVisualStyleBackColor = true;
+            this.btnClassifyWork.Click += new System.EventHandler(this.btnClassifyWork_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 447);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Відсоток правильно клсифікованих об\'єктів:";
+            // 
+            // lblTotalSuccessTest
+            // 
+            this.lblTotalSuccessTest.AutoSize = true;
+            this.lblTotalSuccessTest.Location = new System.Drawing.Point(246, 447);
+            this.lblTotalSuccessTest.Name = "lblTotalSuccessTest";
+            this.lblTotalSuccessTest.Size = new System.Drawing.Size(77, 13);
+            this.lblTotalSuccessTest.TabIndex = 10;
+            this.lblTotalSuccessTest.Text = "Не визначено";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(243, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Введіть дані пацієнтів або завантажте з файлу";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(421, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Вибрати лист:";
+            // 
+            // cboSheetWork
+            // 
+            this.cboSheetWork.FormattingEnabled = true;
+            this.cboSheetWork.Location = new System.Drawing.Point(505, 6);
+            this.cboSheetWork.Name = "cboSheetWork";
+            this.cboSheetWork.Size = new System.Drawing.Size(150, 21);
+            this.cboSheetWork.TabIndex = 10;
+            this.cboSheetWork.SelectedIndexChanged += new System.EventHandler(this.cboSheetWork_SelectedIndexChanged);
+            // 
+            // btnOpenWork
+            // 
+            this.btnOpenWork.Location = new System.Drawing.Point(268, 6);
+            this.btnOpenWork.Name = "btnOpenWork";
+            this.btnOpenWork.Size = new System.Drawing.Size(147, 23);
+            this.btnOpenWork.TabIndex = 11;
+            this.btnOpenWork.Text = "Відкрити файл даних *.xls";
+            this.btnOpenWork.UseVisualStyleBackColor = true;
+            this.btnOpenWork.Click += new System.EventHandler(this.btnOpenWork_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 390);
-            this.Controls.Add(this.btnClassify);
-            this.Controls.Add(this.btnOpen);
-            this.Controls.Add(this.dataGridView);
-            this.Controls.Add(this.cboSheet);
+            this.ClientSize = new System.Drawing.Size(668, 527);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Діагностика післяопераційних ускладень (мітральний клапан)";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvWorkMode)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox cboSheet;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem довідкаToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.DataGridView dataGridView;
-        private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.Button btnClassify;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ComboBox cboSheet;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClassifyWork;
+        private System.Windows.Forms.DataGridView dgvWorkMode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotalSuccessTest;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboSheetWork;
+        private System.Windows.Forms.Button btnOpenWork;
     }
 }
 
--- DiagnosticSystem/Form1.cs
+++ DiagnosticSystem/Form1.cs
@@ -20,7 +20,7 @@ namespace DiagnosticSystem
             InitializeComponent();
         }
 
-        DataSet data;
+        DataSet data, work_data;
 
         DataSet classificators;
 
@@ -40,8 +40,18 @@ namespace DiagnosticSystem
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            data = readDataSetFromExcel("d:/Documents/GitHub/diagnostic-system-csharp/test-data.xls");
+            //data = readDataSetFromExcel("d:/Documents/GitHub/diagnostic-system-csharp/test-data.xls");
             classificators = readDataSetFromExcel("d:/Documents/GitHub/diagnostic-system-csharp/classificators.xls", false);
+            //classificators = readDataSetFromExcel("d:/Documents/GitHub/diagnostic-system-csharp/classificators_2.xls", false);
+
+            dgvWorkMode.Columns.Add("N", "N");
+
+            for(int i = 0; i < 69; i++)
+            {
+                string col_name = "x" + i.ToString();
+                dgvWorkMode.Columns.Add(col_name, col_name);
+            }
+
         }
 
         // ----------------------------------------------------------------------------------
@@ -69,11 +79,27 @@ namespace DiagnosticSystem
             dataGridView.DataSource = data.Tables[cboSheet.SelectedIndex];
         }
 
-        private void btnClassify_Click(object sender, EventArgs e)
+        private int getColIndexByColName(DataTable table, string ColName)
         {
-            DataTable data_table = data.Tables["mitral"];
+            for (int ColIndex = 0; ColIndex < table.Columns.Count; ColIndex++)
+            {
+                if (table.Columns[ColIndex].ColumnName.Equals(ColName))
+                {
+                    return ColIndex;
+                }
+            }
+            return -1;
+        }
 
+        private double classification(DataTable data_table)
+        {
             int[] statistic_success = { 0, 0, 0, 0 };
+
+            int res_col_index = -1;
+
+            data_table.Columns.Add("k-result");
+            res_col_index = getColIndexByColName(data_table, "k-result");
+            
 
             double res_1_123, res_2_134, res_3_124, res_4_123;
             for (int RowIndex = 0; RowIndex < data_table.Rows.Count; RowIndex++)
@@ -84,8 +110,8 @@ namespace DiagnosticSystem
                 res_4_123 = Classification.classify(classificators.Tables["4_123"], data_table, RowIndex);
 
                 int diagnos = 0;
-                
-                if(res_1_123 < res_2_134 &&
+
+                if (res_1_123 < res_2_134 &&
                    res_1_123 < res_3_124 &&
                    res_1_123 < res_4_123) diagnos = 1;
 
@@ -101,6 +127,9 @@ namespace DiagnosticSystem
                    res_4_123 < res_2_134 &&
                    res_4_123 < res_3_124) diagnos = 4;
 
+
+
+                data_table.Rows[RowIndex].SetField<int>(res_col_index, diagnos);
                 double true_diagnos = 0;
                 Classification.getValByRowIndexAndColName(data_table, RowIndex, "k", ref true_diagnos);
 
@@ -111,20 +140,75 @@ namespace DiagnosticSystem
                 if (isSucces)
                     statistic_success[true_diagnos_int - 1] += 1;
 
-                Console.WriteLine("{0}: 1={1}, 2={2}, 3={3}, 4={4}, diagnos={5}, true_dignos={6} >> {7}", 
-                    RowIndex, 
-                    res_1_123, 
-                    res_2_134, 
-                    res_3_124, 
+                Console.WriteLine("{0}: 1={1}, 2={2}, 3={3}, 4={4}, diagnos={5}, true_dignos={6} >> {7}",
+                    RowIndex,
+                    res_1_123,
+                    res_2_134,
+                    res_3_124,
                     res_4_123,
                     diagnos,
                     true_diagnos_int,
                     isSucces);
+
+
+
             }
 
             double total_success = statistic_success.Sum() / Convert.ToDouble(data_table.Rows.Count);
 
             Console.WriteLine("Total Success = {0}", total_success);
+
+            return total_success;
+        }
+
+        private void btnClassify_Click(object sender, EventArgs e)
+        {
+            //DataTable data_table = data.Tables["mitral"];
+            DataTable data_table = (DataTable)dataGridView.DataSource;
+
+            double total_success = classification(data_table);
+
+            lblTotalSuccessTest.Text = String.Format("{0:P}", total_success);
+
+
+        }
+
+        private void btnClassifyWork_Click(object sender, EventArgs e)
+        {
+            DataTable data_table = (DataTable)dgvWorkMode.DataSource;
+
+            double total_success = classification(data_table);
+
+            //lblTotalSuccessTest.Text = String.Format("{0:P}", total_success);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboSheetWork_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dgvWorkMode.DataSource = work_data.Tables[cboSheetWork.SelectedIndex];
+        }
+
+        private void btnOpenWork_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Workbook|*.xls", ValidateNames = true })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    work_data = readDataSetFromExcel(ofd.FileName);
+
+                    cboSheetWork.Items.Clear();
+                    foreach (DataTable dt in work_data.Tables)
+                        cboSheetWork.Items.Add(dt.TableName);
+
+                }
+            }
+
+            dgvWorkMode.Rows.Clear();
+            dgvWorkMode.Columns.Clear();
         }
     }
 }
